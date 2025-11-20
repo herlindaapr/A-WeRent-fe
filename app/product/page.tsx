fb505c8c7461fed3399051ed931a59ed3deaf3bc
Nurlianto Aldi <nurliantoaldi@gmail.com>
2025-11-20 10:34:26 +0700

    Fix star rating; Add review section; make rent fee section sticky

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,21 +1,23 @@
-import { BiChevronLeft, BiSolidStar } from "react-icons/bi";
+import { BiChevronLeft } from "react-icons/bi";
+import { FaStar, FaRegStar } from "react-icons/fa";
+import { MdOutlineThumbUp } from "react-icons/md";
 import Image from "next/image";
 import Link from "next/link";
 
 const ProductPage = () => {
   return (
     <div
-      className="bg-white text-black"
+      className="bg-white text-black relative"
     >
 
-      {/* # navbar */}
+      {/* # navbar section */}
       <div
         className="w-full bg-white py-3.5 px-7 text-black"
       >
         <BiChevronLeft className="text-[24px]" />
       </div>
 
-      {/* image */}
+      {/* image section */}
       <div
         className="relative w-full h-[647px]"
       >
@@ -27,7 +29,7 @@ const ProductPage = () => {
         />
       </div>
 
-      {/* product title and review count */}
+      {/* product description section */}
       <div
         className="flex flex-col bg-white items-start text-black py-[18px] px-[30px] text-[18px] font-semibold leading-[15px]"
       >
@@ -37,20 +39,20 @@ const ProductPage = () => {
           <span>Black Kaftan with <br /> Embellishment</span>
         </div>
 
-        {/* review */}
+        {/* product rating */}
         <div
           className="flex flex-row items-center justify-start gap-[11px] bg-white text-black py-[18px]"
         >
 
           {/* stars */}
           <div
-            className="flex flex-row gap-2"
+            className="flex flex-row gap-2 text-[#FBBF24]"
           >
-            <BiSolidStar />
-            <BiSolidStar />
-            <BiSolidStar />
-            <BiSolidStar />
-            <BiSolidStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaRegStar className="text-gray-400 font-bold"/>
           </div>
 
           {/* review count */}
@@ -62,14 +64,22 @@ const ProductPage = () => {
 
       </div>
 
-      <hr />
+      <div
+        className="px-[30px]"
+      >
+        <div
+          className="w-full h-0.5 bg-[#9B9A9A]"
+        >
 
-      {/* review overview */}
+        </div>
+      </div>
+
+      {/* overall review section */}
       <div
         className="flex flex-col px-[30px] py-[13px] gap-[18px]"
       >
 
-        {/* review text and view more link */}
+        {/* header */}
         <div
           className="flex flex-row items-center justify-between"
         >
@@ -77,24 +87,27 @@ const ProductPage = () => {
           <Link href="#" className="underline">View More &gt;</Link>
         </div>
 
+        {/* stars */}
         <div>
-          {/* stars */}
-          <div className="flex flex- text-[12px] gap-2">
-            <BiSolidStar />
-            <BiSolidStar />
-            <BiSolidStar />
-            <BiSolidStar />
-            <BiSolidStar />
+          <div className="flex flex- text-[12px] gap-2 text-[#FBBF24]">
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaRegStar className="text-gray-400 font-bold"/>
           </div>
         </div>
 
+
+        {/* rating bar */}
         <div
           className=""
         >
-          {/* rating */}
           <div
             className="flex flex-col gap-3.5 text-[12px] w-full"
           >
+
+            {/* small */}
             <div
               className="flex flex-row items-center justify-between gap-4"
             >
@@ -106,7 +119,11 @@ const ProductPage = () => {
               <div
                 className="flex w-8/12 bg-[#D9D9D9] h-[9px]"
               >
-                
+                <div
+                  className="flex w-2/100 bg-[#2B6646]"
+                >
+
+                </div>
               </div>
               <div
                 className="flex items-center justify-end w-1/12"
@@ -114,6 +131,8 @@ const ProductPage = () => {
                 <span>2%</span>
               </div>
             </div>
+
+            {/* true to size */}
             <div
               className="flex flex-row items-center justify-between gap-4"
             >
@@ -125,7 +144,11 @@ const ProductPage = () => {
               <div
                 className="flex w-8/12 bg-[#D9D9D9] h-[9px]"
               >
-                
+                <div
+                  className="flex w-85/100 bg-[#2B6646]"
+                >
+
+                </div>
               </div>
               <div
                 className="flex items-center justify-end w-1/12"
@@ -133,6 +156,8 @@ const ProductPage = () => {
                 <span>85%</span>
               </div>
             </div>
+            
+            {/* large */}
             <div
               className="flex flex-row items-center justify-between gap-4"
             >
@@ -144,7 +169,10 @@ const ProductPage = () => {
               <div
                 className="flex w-8/12 bg-[#D9D9D9] h-[9px]"
               >
-                
+                <div
+                  className="flex w-13/100 bg-[#2B6646]"
+                >
+                </div>
               </div>
               <div
                 className="flex items-center justify-end w-1/12"
@@ -156,9 +184,94 @@ const ProductPage = () => {
         </div>
       </div>
 
+      {/* divider */}
+      <div
+        className="px-[30px]"
+      >
+        <div
+          className="w-full h-0.5 bg-[#9B9A9A]"
+        >
+
+        </div>
+      </div>
+
+      {/* user review section */}
+      <div
+        className="px-[30px] py-[13px]"
+      >
+
+        {/* header */}
+        <div
+          className="flex flex-row items-center justify-between gap-4"
+        >
+
+          {/* photo porfil, star, detail */}
+          <div
+            className="flex flex-row items-center justify-center gap-[15px]"
+          >
+
+            {/* photo profil */}
+            <div
+              className="flex bg-gray-400 h-[29px] w-[29px] rounded-full"
+            >
+              
+            </div>
+
+            {/* star and detail */}
+            <div
+              className="flex flex-col gap-3.5"
+            >
+
+              {/* star */}
+              <div
+                className="flex flex-row gap-[5px] text-[11px] text-[#FBBF24]"
+              >
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+              </div>
+
+              {/* detail */}
+              <div className="text-[9px] text-[#9B9A9A] font-medium">
+                <span>165 CM   65 KG   88 / 78 / 110 CM</span>
+              </div>
+
+            </div>
+          </div>
+
+
+          {/* like button */}
+          <div
+            className="flex flex-row items-center justify-center gap-1.5"
+          >
+            <MdOutlineThumbUp className="text-[20px]"/>
+            <span className="text-[12px]">(5)</span>
+          </div>
+
+        </div>
+
+        {/* user review text */}
+        <div
+          className="flex flex-col gap-[11px] mt-5 text-[12px]"
+        >
+          <p>
+            This black kaftan is a wardrobe staple for me now! The quality is outstanding, and it&apos;s incredibly versatile. I&apos;ve worn it to brunch with friends, to the beach as a cover-up, and even to a formal dinner with the right accessories. It&apos;s so comfortable and easy to style. I can&apos;t recommend it enough! ps. btw im using my boyfriend account xixi!
+          </p>
+        </div>
+
+        {/* user review date */}
+        <div
+          className="mt-[11px] text-[9px] text-[#9B9A9A]"
+        >
+          <span>Nov 29, 2023</span>
+        </div>
+      </div>
+
       {/* product price */}
       <div
-        className="flex flex-row items-start justify-between bg-white px-3.5 py-[11px] text-black"
+        className="flex flex-row items-start justify-between bg-white px-3.5 py-[11px] text- h-[108px] sticky bottom-0 border border-[#2B6646]/20"
       >
         <div
           className="flex flex-col"
